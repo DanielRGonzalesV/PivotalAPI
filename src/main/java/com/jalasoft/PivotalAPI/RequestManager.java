f25da720071917e4970ec57337df66db258a5afb
DanielRGonzalesV <drgvandroid@gmail.com>
2016-07-04 17:28:54 -0400

    EndPoint Method

--- src/main/java/com/jalasoft/PivotalAPI/RequestManager.java
+++ src/main/java/com/jalasoft/PivotalAPI/RequestManager.java
@@ -3,13 +3,17 @@ package com.jalasoft.PivotalAPI;
 import java.util.Map;
 
 import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
 import com.jayway.restassured.specification.RequestSpecification;
+import org.apache.log4j.Logger;
 
 
 public class RequestManager {
     private static final RequestSpecification RESPONSE_SPECIFICATION = Authentication.getInstance ().getRequestSpecification ();
+
+    private static final Logger LOGGER = Logger.getLogger(RequestManager.class.getName());
 
     private RequestManager () {
     }
@@ -20,7 +24,8 @@ public class RequestManager {
 
     public static Response postRequest (String endpoint, Map<String, Object> parameters) {
         JsonParser jsonParser = new JsonParser ();
-        //par = jsonParser.parse(parameters.toString()).getAsJsonObject());//change to logger
+        JsonElement jsonObject = jsonParser.parse(parameters.toString());
+        LOGGER.info("The JSON object is: " + jsonObject);
         return RESPONSE_SPECIFICATION.contentType (ContentType.JSON).body (jsonParser.parse (parameters.toString ())).when ().post (endpoint);
     }
 
@@ -30,6 +35,8 @@ public class RequestManager {
 
     public static Response putRequest(String endpoint, Map<String, Object> parameters ){
         JsonParser jsonParser = new JsonParser ();
+        JsonElement jsonObject = jsonParser.parse(parameters.toString());
+        LOGGER.info("The JSON object is: " + jsonObject);
         return RESPONSE_SPECIFICATION.contentType (ContentType.JSON).body (jsonParser.parse (parameters.toString ())).when ().put(endpoint);
     }
 
